5bfa0e99c18b52e4f46fa1b51b749893c99f134a
wechu07 <wechuli017@gmail.com>
2021-12-20 15:02:21 +0300

    correct logic

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
+
+"""Define a class square"""
+
+
 class Square:
-    """Represents a square.
-    Private instance attribute: size.
-    Instantiation with size (no type/value verification).
-    """
+    """Represent a square."""
 
     def __init__(self, size):
-        """Initializes the data."""
+        """Initialize a new Square.
+        Args:
+            size (int): The size of the new square.
+        """
         self.__size = size
